32e436b104b2bf51904dc20df786cad8d5029a9e
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2025-01-20 08:17:02 +0300

    made it automatically to login and save cookies for multiple accounts

--- get_client.py
+++ get_client.py
@@ -2,49 +2,56 @@ from twikit import Client
 import asyncio
 import json
 import traceback
-CLIENT_CACHE = {}
+
 async def test_account(client,username):
-    try:
-        await client.get_user_tweets(username,"Replies")
-        print(f"Logged in successfully for {username}.")
-        return True
-    except Exception as e:
-        print(f"bad news account suspended: {e}")
+    state = await client._get_user_state()
+    if state == "suspended":
+        print(f"bad news account suspended: {username}")
         return False
+    print(f"Logged in successfully for {username}.")
+    return True
 
 async def get_or_create_client(account, file_path='cookies.json'):
     """Get or create a logged-in client for the given account."""
-    global CLIENT_CACHE
     username = account["username"]
-
-    if username in CLIENT_CACHE:
-        print("Found logged-in client in cache and returned")
-        return CLIENT_CACHE[username]
 
     try:
         client = Client('en-US')
-
         try:
+
             with open(file_path, 'r', encoding='utf-8') as f:
                 cookies = json.load(f) 
-                client.set_cookies(cookies)
-            print("Loaded cookies and initialized client.")
+                cookie = cookies.get(username,None)
+                if cookie:
+                    client.set_cookies(cookie)
+                    print("Loaded cookies and initialized client.")
+                    status = await test_account(client,username)
+                    if not status:
+                        return None
+                else:
+                    raise FileNotFoundError("Saved cookie not found!")
         except FileNotFoundError:
             print("Cookies not found, logging in with credentials.")
             await client.login(
                 auth_info_1=account["username"],
-                auth_info_2=account["phone"],
+                auth_info_2=account["email"],
                 password=account["password"]
                 # totp_secret=account["totp"]  # Uncomment if needed
             )
             print(f"Logged in successfully for {username}.")
+            status  = await test_account(client,username)
 
-            cookies = client.get_cookies()
+            if not status:
+                print(f"bad news account suspended: {e}")
+                return None
+
+            cookie = client.get_cookies()
+            cookies[username] = cookie
+
             with open(file_path, 'w', encoding='utf-8') as f:
-                json.dump(cookies, f)
+                json.dump(cookies, f, indent=2)
             print(f"Cookies saved to {file_path}.")
 
-        CLIENT_CACHE[username] = client
         return client
 
     except Exception as e:
