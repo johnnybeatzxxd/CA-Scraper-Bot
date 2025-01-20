8ab7d2d4c5eb8a1deb807bb06a84364fad98deec
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2025-01-20 08:18:02 +0300

    made it functional for mutiple accounts

--- main.py
+++ main.py
@@ -4,31 +4,27 @@ import os
 from twikit import Client, Tweet
 from get_client import get_or_create_client
 from get_cookies import save_cookies_locally
+from send_message import send_message_to_bot
 import json
 
 load_dotenv()
 
-USERNAME = os.getenv("USERNAME")
-EMAIL = os.getenv("EMAIL") 
-PASSWORD = os.getenv("PASSWORD") 
+TARGET = "elonmusk" 
 
-TARGET = "johnnyxxdpro" 
+CHECK_INTERVAL = 5  
 
-CHECK_INTERVAL = 10  
 
-account = {
-    "username": USERNAME,
-    "email": EMAIL,
-    "password": PASSWORD,
-}
-
-def callback(tweet: Tweet) -> None:
+async def callback(tweet: Tweet) -> None:
     print(tweet.reply_to, tweet.in_reply_to)
     print(f'New tweet posted : {tweet.text}')
+    await send_message_to_bot(your_message=tweet.text)
 
 
 async def get_latest_tweet(user,client) -> Tweet:
-    return await client.get_user_tweets(user.id, "Replies")
+    try:
+        return await client.get_user_tweets(user.id, "Replies")
+    except:
+        return await client.get_user_tweets(user.id, "Replies")
 
 
 def remove_cookie_alternative(filepath, name_to_remove):
@@ -54,33 +50,59 @@ def remove_cookie_alternative(filepath, name_to_remove):
         json.dump(new_data, f, indent=2)
     print(f"Cookie entry for '{name_to_remove}' removed and file saved.")
 
+async def initialize_clients():
+
+    clients = []
+    with open("credintials.json","r") as f:
+        credintials = json.load(f)
+
+    for account in credintials["accounts"]:
+        try:
+            client = await get_or_create_client(account)
+        except Exception as e:
+            print(f"Error initializing client for {account['username']}: {e}")
+            continue
+        if client:
+            clients.append(client)
+    print(len(clients),"clients initialized.")
+    return clients
+
 async def main():
 
-    username = account.get("username")
-    while True:
-        client = await get_or_create_client(account)
-        try:
-            user = await client.get_user_by_screen_name(TARGET)
-            break
-        except Exception as e:
-            print(f"Error:{e}")
-            await asyncio.sleep(10)
-            await remove_cookie_alternative("cookies.json",username)
+    clients = await initialize_clients()
+    num_clients = len(clients)
 
-    print("this is the user: ", user.screen_name)
-    before_tweet = await get_latest_tweet(user,client)
+    index = 0
+    print("user obj request sent with index: ", index)
+    user = await clients[index].get_user_by_screen_name(TARGET)
+    await asyncio.sleep(CHECK_INTERVAL)
+    print("request sent with index: ", index)
+    before_tweet = await get_latest_tweet(user,clients[index])
 
     while True:
+
+        print("waiting for the next check...")
         await asyncio.sleep(CHECK_INTERVAL)
-        latest_tweet = await get_latest_tweet(user,client)
+        if index == num_clients - 1:
+            index = 0
+        else:
+            index = index + 1 
+
+        print("request sent with index: ", index)
+        latest_tweet = await get_latest_tweet(user,clients[index])
         print(latest_tweet)
 
         difference = [item for item in latest_tweet if item not in before_tweet]
 
         if difference:
             for item in difference:
-                tweet = await client.get_tweet_by_id(item.id)
-                callback(tweet)
+                if index == num_clients - 1:
+                    index = 0
+                else:
+                    index = index + 1 
+                print("request sent with index: ", index)
+                tweet = await clients[index].get_tweet_by_id(item.id)
+                await callback(tweet)
 
         before_tweet = latest_tweet
 
