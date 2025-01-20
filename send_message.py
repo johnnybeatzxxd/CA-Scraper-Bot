bd0768f252d19ce8f335c67dc97e9ddd7bd70564
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2025-01-20 16:55:36 +0300

    added telethon to send messages to telegram

---
+++ send_message.py
@@ -0,0 +1,26 @@
+from telethon import TelegramClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+PHONE_NUMBER = os.getenv("PHONE_NUMBER")  
+
+client = TelegramClient("user_session", API_ID, API_HASH)
+
+async def send_message_to_bot(bot_username: str = "johnnybeatz", your_message: str = "Hello, Bot!") -> None:
+    await client.start(phone=PHONE_NUMBER)
+
+    bot_username = "BotFather"
+
+    await client.send_message(bot_username, your_message)
+    print("Message sent!")
+
+    await client.disconnect()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(send_message_to_bot())
+
